44bb4bb0c9eab719bf3b5c227fc31614284a5f91
Juanda0 <juanda20202@hotmail.com>
2021-05-20 16:43:15 -0500

    Cambios visuales

--- swd/Procesamiento de imagenes/filtros.py
+++ swd/Procesamiento de imagenes/filtros.py
@@ -3,7 +3,6 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 img = cv2.imread('metal1.jpg',0)
-print(img)
 # Blurring 
 blurred = cv2.bilateralFilter(img,15,150,150)
 img = np.uint8(img)
@@ -20,12 +19,18 @@ high, thresh_im = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU
 low = 0.5*high
 edgesNormal = cv2.Canny(img,low,high)
 
-finalimg = img - edgesBlurred
-#primer parametro es imagen a mostrar
-plt.subplot(121),plt.imshow(edgesNormal,cmap = 'gray')
-plt.title('EdgesNormal Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(blurred,cmap = 'gray')
-plt.title('EdgesBlurred Image'), plt.xticks([]), plt.yticks([])
 
+"""
+#Normal - Normal Edges
+plt.subplot(121),plt.imshow(img, cmap= 'gray')
+plt.title('Normal Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edgesNormal, cmap= 'gray')
+plt.title('Edges Image'), plt.xticks([]), plt.yticks([])
+"""
+#Blurred - Blurred Edges
+plt.subplot(121),plt.imshow(blurred, cmap= 'gray')
+plt.title('Blurred Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edgesBlurred, cmap= 'gray')
+plt.title('Edges Image'), plt.xticks([]), plt.yticks([])
 
 plt.show()
